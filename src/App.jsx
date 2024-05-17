cd481407c2731ceb8b397b9e3931971447d16fa3
Diana Rukavishnikova <psihomatika@gmail.com>
2024-05-17 08:22:27 +0200

    feat: add validation to form

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import SomeInput from "../src/components/SomeInput";
+// import Form from "../src/components/Form";
+
+function App() {
+  return (
+    <div className="app">
+      <SomeInput />
+      {/* <Form /> */}
+    </div>
+  );
+}
+
+export default App;
